a17ca8498d185fedf6223e824be2c20249b1b90c
Ian Heraty <heratyian@gmail.com>
2024-10-22 10:45:21 -0500

    Revert indent in movies controller private methods.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -58,13 +58,12 @@ class MoviesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_movie
-      @movie = Movie.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def movie_params
-      params.require(:movie).permit(:title, :year, :director_id)
-    end
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :year, :director_id)
+  end
 end
