227e9a3eb84e5c4d07cb7a9aacd82ea3159ebd6a
Ian Heraty <heratyian@gmail.com>
2024-11-11 13:38:22 -0600

    Refactor model and controller functionality into concerns.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,5 @@
 class ApplicationController < ActionController::Base
-  include Pundit::Authorization
+  include Deviseable, Punditable
 
   skip_forgery_protection
-
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar])
-  end
 end
