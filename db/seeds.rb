76141ab629dc0a939d7551b3fddbd01ff985a878
Ian Heraty <heratyian@gmail.com>
2024-11-26 14:36:56 -0600

    Add admin using seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(
+  email: "ihera2@uillinois.edu",
+  password: Rails.application.credentials.admin_password,
+  admin: true
+)
